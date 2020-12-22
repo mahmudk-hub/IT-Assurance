2b7e73682ebd0d25c4c2f58046b66a5574d84ad6
kmahmud <kmahmud@mahmudhp>
2020-12-22 17:58:04 -0500

    package and class created

---
+++ it_assurance/src/main/java/it_assurance/Test3.java
@@ -0,0 +1,9 @@
+package it_assurance;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ it_assurance/src/test/java/it_assurance/Test4.java
@@ -0,0 +1,10 @@
+package it_assurance;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome");
+
+	}
+
+}
